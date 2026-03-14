42fb58ea9ccf0571b347f1c80f868e29bb702cbe
TrripeR <malacor5@gmail.com>
2026-03-14 20:27:08 +0100

    ready for deploy?

--- easy_meal_challenge/settings.py
+++ easy_meal_challenge/settings.py
@@ -79,23 +79,23 @@ WSGI_APPLICATION = 'easy_meal_challenge.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "easy_meal_challenge",
-        "USER": "postgres",
-        "PASSWORD": "12345",
-        "HOST": "localhost",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "easy_meal_challenge",
+#         "USER": "postgres",
+#         "PASSWORD": "12345",
+#         "HOST": "localhost",
+#         "PORT": "5432",
+#     }
+# }
 
 
 # Password validation
